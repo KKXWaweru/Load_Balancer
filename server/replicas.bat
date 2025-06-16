917a226fff5ac3b025a6765fac94d54493089adc
unknown <kkxwaweru@gmail.com>
2025-06-17 00:05:19 +0300

    Added batch files for the server

---
+++ server/replicas.bat
@@ -0,0 +1,12 @@
+D:\Documents\4th Year\Distributed Systems\Load_balancer_assignment\Load_Balancer\server>replicas.bat
+{
+  "message": {
+    "N": 3,
+    "replicas": [
+      "1",
+      "2",
+      "3"
+    ]
+  },
+  "status": "successful"
+}
